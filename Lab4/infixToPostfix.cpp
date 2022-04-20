02d1995722cde215a395a3f4afda991cc2192871
Muhammad Hameed <muhammadhameed105@gmail.com>
2022-04-20 20:58:32 +0500

    precedence added

--- Lab4/infixToPostfix.cpp
+++ Lab4/infixToPostfix.cpp
@@ -1,3 +1,25 @@
+int precedence(char c)
+{
+    if(c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
 string infixToPostfix(string exp)
 {
     string postfix;
